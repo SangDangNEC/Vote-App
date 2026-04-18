fac16c568e29b71113074c2114b5321bb4542979
goog49296-code <goog49296@gmail.com>
2026-04-18 21:35:12 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('votecalc-cache').then(function(cache) {
+            return cache.addAll([
+                './',
+                './index.html'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
